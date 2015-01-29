2920863316dc44d3e2da435eef49bfc71d967b23
Demyan Rogozhin <demyan.rogozhin@gmail.com>
2015-01-29 20:33:37 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,24 +1,22 @@
 Package.describe({
-    name:"natestrauser:accounts-saml",
-    summary: "saml login provider for meteor",
-  version: "0.1.1",
-  git: "https://github.com/nate-strauser/meteor-accounts-saml.git"
+    name:'drogozhin:meteor-accounts-saml-ad',
+    summary: 'ActiveDirectory (SAML) login provider for meteor',
+    version: '0.2.0',
+    summary: 'Based on natestrauser:accounts-saml by Nathan Strauser',
+    git: 'https://github.com/demyanrogozhin/meteor-accounts-saml.git'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom("METEOR@0.9.0");
+    api.versionsFrom('METEOR@0.9.0');
     api.use(['routepolicy','webapp','underscore', 'service-configuration'], 'server');
     api.use(['http','accounts-base'], ['client', 'server']);
-
     api.add_files(['saml_server.js','saml_utils.js'], 'server');
-	api.add_files('saml_client.js', 'client');
+    api.add_files('saml_client.js', 'client');
 });
 
 Npm.depends({
-	"xml2js": "0.2.0",
-    "xml-crypto": "0.0.20",
-    "xmldom": "0.1.6",
-  "connect": "2.7.10"
+    'xml2js': '0.2.0',
+    'xml-crypto': '0.0.20',
+    'xmldom': '0.1.6',
+    'connect': '2.7.10'
 });
-
-
