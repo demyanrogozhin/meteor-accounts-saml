36cc02b2a4028d6562dda6558b578a27122245cb
Demyan Rogozhin <demyan.rogozhin@gmail.com>
2015-02-02 20:39:49 +0300

    Bump version number

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name:'drogozhin:meteor-accounts-saml-ad',
     summary: 'ActiveDirectory (SAML) login provider for meteor',
-    version: '0.2.0',
+    version: '0.2.1',
     summary: 'Based on natestrauser:accounts-saml by Nathan Strauser',
     git: 'https://github.com/demyanrogozhin/meteor-accounts-saml.git'
 });
